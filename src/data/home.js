f24d32a0824b04980bbee646627878f5a9780443
Thomas Brace <tommy-brace-22@hotmail.com>
2019-04-08 23:06:38 +0100

    Add colour scheme and about page

--- src/data/home.js
+++ src/data/home.js
@@ -1,20 +1,15 @@
 export const homeData = {
-  header: "Hi I'm Tom and welcome to my website!",
+  header: "Tom Brace, Front End React and Wordpress Developer",
   description: [
     {
       link: "/about",
       hoverText: "Learn more about me",
-      text: "I am a 22 year old web and app developer from Hartlepool, England,"
+      text: "I am a 22 year old web and app developer"
     },
     {
       link: "/projects",
       hoverText: "See what I've been working on",
       text: "I work in Newcastle for Komodo Digital"
-    },
-    {
-      link: "/blog",
-      hoverText: "Read more in my blog",
-      text: "And finally I'm a Durham University Computer Science Graduate"
     }
   ]
 };
